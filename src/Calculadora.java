bd2a4c32f1a77ae56551a13005f0cf08222c81d7
Cristiano Santos <devcristianobh@gmail.com>
2026-03-23 22:28:18 -0300

    Removing old files, keeping only the Calculator

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,16 +1,16 @@
-import java.util.Scanner; // importa no topo do arquivo
+import java.util.Scanner;
 
-public class MeuSegundoPrograma {
+public class Calculadora {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // cria o scanner
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite um número:");
-        double numero1 = scanner.nextDouble(); // lê o que o usuário digitou
+        double numero1 = scanner.nextDouble();
 
         System.out.println("Você digitou: " + numero1);
 
         System.out.println("Digite outro número:");
-        double numero2 = scanner.nextDouble(); // lê o que o usuário digitou
+        double numero2 = scanner.nextDouble();
         System.out.println("Você digitou: " + numero2);
 
         System.out.println("Digite a operação (+, -, *, /)");
